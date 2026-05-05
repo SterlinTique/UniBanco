25c31633d163e22114e9530fcfe5a64a9f066c9b
SterlinTique <jader.tique@utp.edu.co>
2026-05-05 16:43:22 -0500

    chore(firebase): agregar dependencias de Firebase Realtime Database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,5 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
 }
