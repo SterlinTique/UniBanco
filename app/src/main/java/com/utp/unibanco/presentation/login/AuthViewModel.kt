1ffd2071f001ef5777a3e6a4abd3e462b46fa52f
SterlinTique <jader.tique@utp.edu.co>
2026-05-30 23:45:40 -0500

    feat(auth/register): implementar validaciones robustas y manejo de alertas

--- app/src/main/java/com/utp/unibanco/presentation/login/AuthViewModel.kt
+++ app/src/main/java/com/utp/unibanco/presentation/login/AuthViewModel.kt
@@ -12,6 +12,20 @@ class AuthViewModel(
             onResult(false, R.string.error_empty_fields)
             return
         }
+
+        if (!document.all { it.isDigit() }) {
+            onResult(false, R.string.error_invalid_document)
+            return
+        }
+        if (document.length < 8) {
+            onResult(false, R.string.error_document_length)
+            return
+        }
+        if (password.length < 6) {
+            onResult(false, R.string.error_password_length)
+            return
+        }
+
         loginUseCase(document, password, onResult)
     }
 }
